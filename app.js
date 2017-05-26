adf8354bfab82c297af098f749968243e0dcef7b
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-26 16:06:50 -0500

    Adding handler

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function (request, response) {
     response.send('It is working!')
 });
 
+app.post('/handler', function (request, response) {
+    console.log('request', request);
+    response.send('It is working!')
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('App is listening on port 3000!')
 });
