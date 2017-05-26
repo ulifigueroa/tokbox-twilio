54cb660d842bfd48a3c7b9d9423bfb3ba09893a2
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-26 16:43:01 -0500

    Using VoiceReponse constructor.

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.get('/', function (request, response) {
 });
 
 app.post('/handler', function (request, response) {
-    let twiml = new twilio.TwimlResponse();
-    twiml.say('Welcome to your Live interview, please enter the code.', { voice: 'alice' });
+    // Use the Twilio Node.js SDK to build an XML response
+    const twiml = new VoiceResponse();
+    twiml.say({voice: 'alice'}, 'Welcome to your Live interview, please enter the code.');
+
+    // Render the response as XML in reply to the webhook request
     response.type('text/xml');
     response.send(twiml.toString());
 });
