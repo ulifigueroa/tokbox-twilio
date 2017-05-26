50105f9ed591e58e66a7bf004968bd901ce28f28
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-26 18:05:20 -0500

    Separating voice from gather.

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ app.get('/', function (request, response) {
     response.send('It is working!')
 });
 
-app.post('/handler', function (request, response) {
+app.post('/voice', function (request, response) {
+    const gather = twiml.gather({numDigits: 4, action: '/gather'});
+
+    gatherNode.say({voice: 'alice'}, 'Welcome to your Live interview, please enter the four digits code.');
+    twiml.redirect('/voice');
+
+    response.type('text/xml');
+    response.send(twiml.toString());
+});
+
+app.post('/gather', function (request, response) {
     const twiml = new VoiceResponse();
-    const gatherNode = twiml.gather({numDigits: 4, action: '/handler'});
 
-    console.log('Digits', request.body);
-
-    if (request.body && request.body.Digits) {
+    if (request.body.Digits) {
         twiml.say({voice: 'alice'}, 'I\'m connecting you to the Live Interview, please wait a moment.');
         twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
     } else {
-        gatherNode.say({voice: 'alice'}, 'Welcome to your Live interview, please enter the four digits code.');
-        twiml.redirect('/handler');
+        twiml.redirect('/voice');
     }
 
     response.type('text/xml');
