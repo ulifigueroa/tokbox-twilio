cfb919e4eeeffe62aced7b3f30004fcdc80ade0f
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-29 17:23:13 -0500

    Using opentok to dial

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
+var wepow = {};
 var express = require('express');
 var parser = require('body-parser');
-var requestClient = require('request');
 var app = express();
-
 var OpenTok = require('opentok');
 var opentok = new OpenTok(process.env.OPENTOK_API_KEY, process.env.OPENTOK_API_SECRET);
 var VoiceResponse = require('twilio').twiml.VoiceResponse;
@@ -14,7 +13,11 @@ app.get('/', function (request, response) {
     response.send('It is working!');
 });
 
-app.post('/voice', function (request, response) {
+app.post('/sip', function (request, response) {
+    console.log('SIP connection');
+});
+
+app.post('/pstn', function (request, response) {
     var twiml = new VoiceResponse();
     var gather = twiml.gather({numDigits: 4, action: '/gather'});
 
@@ -31,54 +34,7 @@ app.post('/gather', function (request, response) {
     if (request.body.Digits) {
         twiml.say({voice: 'alice'}, 'I\'m connecting you to the Live Interview, please wait a moment.');
         twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
-
-        var token = opentok.generateToken(process.env.OPENTOK_SESSION_ID);
-
-        var options = {
-            url: 'https://api.opentok.com/v2/project/' + process.env.OPENTOK_API_KEY + '/dial',
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-OPENTOK-AUTH': {
-                    iss: process.env.OPENTOK_API_KEY,
-                    ist: 'project',
-                    iat: Date.now(),
-                    exp: Date.now() + (60 * 1000),
-                    jti: 'jwt_nonce'
-                }
-            },
-            form: {
-                sessionId: process.env.OPENTOK_SESSION_ID,
-                token: token,
-                sip: {
-                    uri: 'sip:test@wepow-live.sip.twilio.com',
-                    /*headers: {
-                      'headerKey': 'headerValue'
-                    },
-                    auth: {
-                      'username': 'username',
-                      'password': 'password'
-                  },*/
-                    secure: false
-                }
-            }
-        };
-
-        console.log('==================================');
-        console.log('Requesting');
-
-        requestClient.post(options, function (error, response, body) {
-            console.log('==================================');
-            if (!error && response.statusCode == 200) {
-                console.log('Sucess: ', response, body);
-            } else {
-                console.log('Status: ', response.statusCode);
-                console.log('Error: ', error);
-                console.log('Body: ', body);
-            }
-            console.log('==================================');
-        });
+        wepow.requestSIPCall();
     } else {
         twiml.redirect('/voice');
     }
@@ -86,6 +42,24 @@ app.post('/gather', function (request, response) {
     response.type('text/xml');
     response.send(twiml.toString());
 });
+
+wepow.requestSIPCall = function() {
+    var token = opentok.generateToken(process.env.OPENTOK_SESSION_ID);
+
+    opentok.dial(process.env.OPENTOK_SESSION_ID, token, process.env.TWILIO_SIP_URI, {
+        auth: {
+            username: process.env.TWILIO_SIP_USER,
+            password: process.env.TWILIO_SIP_PASSWORD,
+        },
+        headers: {}
+    }, function (err, sipCall) {
+        if (err) {
+            console.error(err);
+        }
+
+        console.dir(sipCall);
+    });
+};
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('App is listening on port 3000!')
