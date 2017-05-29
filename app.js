d2d64120c187ac6fcc61e7dd64d3f4f3ecb2d783
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-29 14:15:53 -0500

    Requesting Tokbox SIP interconnect.

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express');
-const parser = require('body-parser')
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-const app = express();
+var express = require('express');
+var parser = require('body-parser');
+var request = require('request');
+var app = express();
+
+var OpenTok = require('opentok');
+var opentok = new OpenTok(process.env.OPENTOK_API_KEY, process.env.OPENTOK_API_SECRET);
+var VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 app.use(parser.urlencoded({extended: false}));
 app.use(parser.json());
 
 app.get('/', function (request, response) {
-    response.send('It is working!')
+    response.send('It is working!');
 });
 
 app.post('/voice', function (request, response) {
-    const twiml = new VoiceResponse();
-    const gather = twiml.gather({numDigits: 4, action: '/gather'});
+    var twiml = new VoiceResponse();
+    var gather = twiml.gather({numDigits: 4, action: '/gather'});
 
     gather.say({voice: 'alice'}, 'Welcome to your Live interview, please enter the four digits code.');
     twiml.redirect('/voice');
@@ -22,11 +26,51 @@ app.post('/voice', function (request, response) {
 });
 
 app.post('/gather', function (request, response) {
-    const twiml = new VoiceResponse();
+    var twiml = new VoiceResponse();
 
     if (request.body.Digits) {
         twiml.say({voice: 'alice'}, 'I\'m connecting you to the Live Interview, please wait a moment.');
         twiml.play({}, 'https://demo.twilio.com/docs/classic.mp3');
+
+        var token = opentok.generateToken(process.env.OPENTOK_SESSION_ID);
+
+        var options = {
+            url: 'https://api.opentok.com/v2/project/' + process.env.OPENTOK_API_KEY + '/dial',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-OPENTOK-AUTH': {
+                    iss: process.env.OPENTOK_API_KEY,
+                    ist: 'project',
+                    iat: Date.now(),
+                    exp: Date.now() + (60 * 1000),
+                    jti: 'jwt_nonce'
+                }
+            }
+        };
+
+        var body = {
+            sessionId: process.env.OPENTOK_SESSION_ID,
+            token: token,
+            sip: {
+                uri: 'sip:test@wepow-live.sip.twilio.com',
+                headers: {
+                  'headerKey': 'headerValue'
+                },
+                auth: {
+                  'username': 'username',
+                  'password': 'password'
+                },
+                secure: false
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                // Print out the response body
+                console.log(body);
+            }
+        });
     } else {
         twiml.redirect('/voice');
     }
