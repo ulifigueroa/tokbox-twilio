f776f77fe5d1c5c46b6c54cec8ebc6cdc0032c11
Ulises Figueroa <ulifigueroa@gmail.com>
2017-05-30 12:46:04 -0500

    Adding name to queue

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', function (request, response) {
 app.post('/sip', function (request, response) {
     var twiml = new VoiceResponse();
 
-    twiml.dial().queue({workflowSid: 'live_interview'});
+    twiml.dial().queue({}, process.env.OPENTOK_SESSION_ID);
 
     console.log('SIP called received.');
     console.log(twiml.toString());
@@ -70,7 +70,7 @@ app.post('/wait', function (request, response) {
 app.post('/enqueue', function (request, response) {
     var twiml = new VoiceResponse();
 
-    twiml.enqueue({workflowSid: 'live_interview', waitUrl: '/wait'});
+    twiml.enqueue({waitUrl: '/wait'}, process.env.OPENTOK_SESSION_ID);
 
     console.log('Adding call to the queue.');
     console.log(twiml.toString());
